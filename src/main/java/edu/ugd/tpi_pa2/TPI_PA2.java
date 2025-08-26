c26acf0722e6db13711309645cd0c32e1aea093a
Cristolfe  Wow <wowleoczk@gmail.com>
2025-08-26 12:26:34 -0300

    asd
    asd

---
+++ src/main/java/edu/ugd/tpi_pa2/TPI_PA2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.ugd.tpi_pa2;
+
+/**
+ *
+ * @author wowle
+ */
+public class TPI_PA2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/edu/ugd/tpi_pa2/modelo/Voluntario.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.ugd.tpi_pa2.modelo;
+
+/**
+ *
+ * @author wowle
+ */
+public class Voluntario {
+    
+}
