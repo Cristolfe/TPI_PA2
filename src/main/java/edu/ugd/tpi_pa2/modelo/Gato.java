f4fa7a1dca29a2407a2e758437608d75f29660f6
Cristolfe  Wow <wowleoczk@gmail.com>
2025-08-26 12:30:19 -0300

    dsa
    asd

--- src/main/java/edu/ugd/tpi_pa2/modelo/Gato.java
+++ src/main/java/edu/ugd/tpi_pa2/modelo/Gato.java
@@ -9,6 +9,22 @@ package edu.ugd.tpi_pa2.modelo;
  * @author wowle
  */
 public class Gato {
+    private String nombre;
+    private String color;
+
+    public Gato(String nombre, String color) {
+        this.nombre = nombre;
+        this.color = color;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getColor() {
+        return color;
+    }
+    
     
     
     
